b7fc50875a8f956cc453d8047b8e6d4a5e0bb1be
Joshua Fontany <joshua.fontany@gmail.com>
2024-04-30 18:29:14 -0700

    html advanced tests start to pass

--- run_tests.sh
+++ run_tests.sh
@@ -11,35 +11,58 @@ PATTERN=$1
 
 # Define the root directory for VSCode syntaxes
 VSCODE_EXTROOT="${HOME}/AppData/Local/Programs/Microsoft VS Code/resources/app/extensions"
+TMGRAMMAR_ROOT="./node_modules/tm-grammars/grammars"
 
 # Run the grammar test with the provided pattern
 npx vscode-tmgrammar-test \
     -g syntaxes/tiddlywiki5.tmLanguage.json \
-    -g "${VSCODE_EXTROOT}/coffeescript/syntaxes/coffeescript.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/cpp/syntaxes/c.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/cpp/syntaxes/cpp.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/csharp/syntaxes/csharp.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/css/syntaxes/css.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/git-base/syntaxes/git-commit.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/html/syntaxes/html.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/java/syntaxes/java.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/javascript/syntaxes/JavaScript.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/json/syntaxes/JSON.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/make/syntaxes/make.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/markdown-basics/syntaxes/markdown.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/objective-c/syntaxes/objective-c.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/objective-c/syntaxes/objective-c++.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/perl/syntaxes/Perl.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/php/syntaxes/html.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/php/syntaxes/php.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/python/syntaxes/MagicPython.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/razor/syntaxes/cshtml.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/ruby/syntaxes/ruby.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/shellscript/syntaxes/Shell-Unix-Bash.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/sql/syntaxes/sql.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/xml/syntaxes/xml.tmLanguage.json" \
-    -g "${VSCODE_EXTROOT}/xml/syntaxes/xsl.tmLanguage.json" \
-    -g "./node_modules/language-gfm/grammars/gfm.json" \
-    -g "./node_modules/vscode-jsp/syntaxes/jsp.tmLanguage.json" \
-    -g "./node_modules/vscode-jsp/syntaxes/jsp.tmLanguage.json" \
+    -g "${TMGRAMMAR_ROOT}/asm.json" \
+    -g "${TMGRAMMAR_ROOT}/coffee.json" \
+    -g "${TMGRAMMAR_ROOT}/c.json" \
+    -g "${TMGRAMMAR_ROOT}/clojure.json" \
+    -g "${TMGRAMMAR_ROOT}/cpp.json" \
+    -g "${TMGRAMMAR_ROOT}/cpp-macro.json" \
+    -g "${TMGRAMMAR_ROOT}/csharp.json" \
+    -g "${TMGRAMMAR_ROOT}/css.json" \
+    -g "${TMGRAMMAR_ROOT}/docker.json" \
+    -g "${TMGRAMMAR_ROOT}/diff.json" \
+    -g "${TMGRAMMAR_ROOT}/erlang.json" \
+    -g "${TMGRAMMAR_ROOT}/elixir.json" \
+    -g "${TMGRAMMAR_ROOT}/elm.json" \
+    -g "${TMGRAMMAR_ROOT}/git-commit.json" \
+    -g "${TMGRAMMAR_ROOT}/glsl.json" \
+    -g "${TMGRAMMAR_ROOT}/go.json" \
+    -g "${TMGRAMMAR_ROOT}/graphql.json" \
+    -g "${TMGRAMMAR_ROOT}/haskell.json" \
+    -g "${TMGRAMMAR_ROOT}/html.json" \
+    -g "${TMGRAMMAR_ROOT}/java.json" \
+    -g "${TMGRAMMAR_ROOT}/javascript.json" \
+    -g "${TMGRAMMAR_ROOT}/json.json" \
+    -g "${TMGRAMMAR_ROOT}/jsx.json" \
+    -g "${TMGRAMMAR_ROOT}/julia.json" \
+    -g "${TMGRAMMAR_ROOT}/kotlin.json" \
+    -g "${TMGRAMMAR_ROOT}/lua.json" \
+    -g "${TMGRAMMAR_ROOT}/make.json" \
+    -g "${TMGRAMMAR_ROOT}/markdown.json" \
+    -g "${TMGRAMMAR_ROOT}/objective-c.json" \
+    -g "${TMGRAMMAR_ROOT}/objective-cpp.json" \
+    -g "${TMGRAMMAR_ROOT}/perl.json" \
+    -g "${TMGRAMMAR_ROOT}/php.json" \
+    -g "${TMGRAMMAR_ROOT}/python.json" \
+    -g "${TMGRAMMAR_ROOT}/r.json" \
+    -g "${TMGRAMMAR_ROOT}/raku.json" \
+    -g "${TMGRAMMAR_ROOT}/razor.json" \
+    -g "${TMGRAMMAR_ROOT}/ruby.json" \
+    -g "${TMGRAMMAR_ROOT}/rust.json" \
+    -g "${TMGRAMMAR_ROOT}/scala.json" \
+    -g "${TMGRAMMAR_ROOT}/shellscript.json" \
+    -g "${TMGRAMMAR_ROOT}/shellsession.json" \
+    -g "${TMGRAMMAR_ROOT}/sql.json" \
+    -g "${TMGRAMMAR_ROOT}/swift.json" \
+    -g "${TMGRAMMAR_ROOT}/toml.json" \
+    -g "${TMGRAMMAR_ROOT}/tsx.json" \
+    -g "${TMGRAMMAR_ROOT}/typescript.json" \
+    -g "${TMGRAMMAR_ROOT}/xml.json" \
+    -g "${TMGRAMMAR_ROOT}/xsl.json" \
+    -g "${TMGRAMMAR_ROOT}/yaml.json" \
     "${PATTERN}"
